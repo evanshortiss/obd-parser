ca9069fee0ed26b5a596f13148c0b8ef8c984b7a
Evan Shortiss <evanshortiss@gmail.com>
2016-02-23 22:14:13 -0500

    Add code coverage to scripts and ci

--- scripts/gen-supported-list.js
+++ scripts/gen-supported-list.js
@@ -7,4 +7,4 @@ fs.readdirSync(path.join(__dirname, '../lib/pids/pids'))
   .forEach(function (fname) {
     var mod = require(path.join(__dirname, '../lib/pids/pids/', fname));
     console.log('* %s (%s)', mod.opts.constname, mod.opts.pid);
-  })
+  });
